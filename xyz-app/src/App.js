2750fef6022a25c0e89bb726b89e5c12535bb5ae
umar <38881409+UmarKasan@users.noreply.github.com>
2023-07-08 15:35:50 +0800

    New

---
+++ xyz-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './assets/xyz_logo.png';
+import './App.css';
+import Navbar from './navbar';
+import Car from './car';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <Car></Car>
+    </div>
+  );
+}
+
+export default App;
---
+++ xyz-app/src/navbar.js
@@ -0,0 +1,17 @@
+import Reacteact from "react";
+import logo from './assets/xyz_logo.png';
+
+function Navbar(){
+    return (
+        <div>
+            {/* <nav class="navbar navbar-light bg-light">
+                <a class="navbar-brand" href="#">
+                    <img src={logo} width="30" height="30" alt=""/>
+                </a>
+            </nav> */}
+            <h1>Hi</h1>
+        </div>
+    )
+}
+
+export default Navbar;
